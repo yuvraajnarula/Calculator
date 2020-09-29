fac32c47fa49ea42762d9baf2049a58df17fe697
unknown <ynarula678@gmail.com>
2020-09-29 10:17:05 +0530

    GUI window

--- calc.py
+++ calc.py
@@ -29,3 +29,24 @@ def clear():
 	global expression 
 	expression = "" 
 	equation.set("") 
+if __name__ == "__main__": 
+	# create a GUI window 
+	gui = Tk() 
+
+	# background colour of GUI window 
+	gui.configure(background="white") 
+
+	# title of GUI window 
+	gui.title("Simple Calculator") 
+
+	#  configuration of GUI window 
+	gui.geometry("270x150") 
+	# we create an instance of this class 
+	equation = StringVar() 
+
+	# create the text entry box for 
+	expression_field = Entry(gui, textvariable=equation)
+	expression_field.grid(columnspan=4, ipadx=70) 
+
+	equation.set('enter your expression') 
+
