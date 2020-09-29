d6ab311d1b8bf5f8160bff567a6507b2809c8017
unknown <ynarula678@gmail.com>
2020-09-29 10:01:01 +0530

    Error fetch using try and except

---
+++ calc.py
@@ -0,0 +1,26 @@
+# calculator using Tkinter 
+
+# import everything from tkinter module 
+from tkinter import *
+
+expression = "" 
+def press(num): 
+	global expression 
+
+	expression = expression + str(num) 
+
+	equation.set(expression) 
+
+
+
+def equalpress(): 
+	try: 
+		global expression 
+		total = str(eval(expression)) 
+
+		equation.set(total) 
+		expression = "" 
+	except: 
+
+		equation.set(" error ") 
+		expression = ""
