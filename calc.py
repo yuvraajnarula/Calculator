31afea5683ef682f936fe3b5f034c8b0e6238a7b
unknown <ynarula678@gmail.com>
2020-09-29 10:58:56 +0530

    Buttons

--- calc.py
+++ calc.py
@@ -50,3 +50,43 @@ if __name__ == "__main__":
 
 	equation.set('enter your expression') 
 
+	# Buttons 
+	button1 = Button(gui, text=' 1 ', fg='black', bg='#9abaed', 
+					command=lambda: press(1), height=1, width=7) 
+	button1.grid(row=2, column=0) 
+
+	button2 = Button(gui, text=' 2 ', fg='black', bg='#9abaed', 
+					command=lambda: press(2), height=1, width=7) 
+	button2.grid(row=2, column=1) 
+
+	button3 = Button(gui, text=' 3 ', fg='black', bg='#9abaed', 
+					command=lambda: press(3), height=1, width=7) 
+	button3.grid(row=2, column=2) 
+
+	button4 = Button(gui, text=' 4 ', fg='black', bg='#9abaed', 
+					command=lambda: press(4), height=1, width=7) 
+	button4.grid(row=3, column=0) 
+
+	button5 = Button(gui, text=' 5 ', fg='black', bg='#9abaed', 
+					command=lambda: press(5), height=1, width=7) 
+	button5.grid(row=3, column=1) 
+
+	button6 = Button(gui, text=' 6 ', fg='black', bg='#9abaed', 
+					command=lambda: press(6), height=1, width=7) 
+	button6.grid(row=3, column=2) 
+
+	button7 = Button(gui, text=' 7 ', fg='black', bg='#9abaed', 
+					command=lambda: press(7), height=1, width=7) 
+	button7.grid(row=4, column=0) 
+
+	button8 = Button(gui, text=' 8 ', fg='black', bg='#9abaed', 
+					command=lambda: press(8), height=1, width=7) 
+	button8.grid(row=4, column=1) 
+
+	button9 = Button(gui, text=' 9 ', fg='black', bg='#9abaed', 
+					command=lambda: press(9), height=1, width=7) 
+	button9.grid(row=4, column=2) 
+
+	button0 = Button(gui, text=' 0 ', fg='black', bg='#9abaed', 
+					command=lambda: press(0), height=1, width=7) 
+	button0.grid(row=5, column=0) 
